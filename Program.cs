badd55242176483f5d7fe3998f5e5fef42801079
Karimjon0 <abidjonovkarimjon@gmail.com>
2024-03-06 14:41:54 +0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,55 +1,103 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-
-// using SimpleCalculator1.Classes;
-
-// Person person = new Person();
-
-// person.b = 12;
-// person.a = 20;
-// person.belgi = "-";
-// person.Amallar();
-
-
-
-
+﻿
 
 
 hi:
+salom:
+System.Console.WriteLine("\nMenyu -----------------------------------------------\n\nAsosiy        |        Chizish      |      Calculator\n-----------------------------------------------------");
+string menyu = Console.ReadLine();
 
-System.Console.Write("Enter a number: ");
-int a = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Enter b number: ");
-int b = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Amalni kiriting: ");
-string belgi = Console.ReadLine();
+if(menyu == "1"){
+    System.Console.WriteLine("Sizga nimani topish kerak?\n-------------------------------------\n");
+    System.Console.WriteLine("Kubning hajmini   |   Turtburchakning P   |   Uchburchakning S   |   ........");
+    string yunalish = Console.ReadLine();
 
-
-
-if(belgi == "+"){
-    System.Console.WriteLine($"{a} + {b} = {a + b}");
+    if(yunalish == "1"){
+        System.Console.WriteLine("Kubning tomonlarini kiriting\n");
+        System.Console.Write("A: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("B: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("C: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write($"Kubingizning hajmi: {a * b * c}");
+    }
+    else if(yunalish == "2"){
+        System.Console.WriteLine("Turtburchak tomonlarini kiriting\n");
+        System.Console.Write("A: ");
+        int a1 = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("B: ");
+        int b1 = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine($"Turtburchakingizning Perimetri: {(a1 + b1)* 2}");
+    }
+    else if(yunalish == "3"){
+        System.Console.WriteLine("Uchburchak tomonlarini kiriting\n");
+        System.Console.Write("A: ");
+        int a2 = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("B: ");
+        int b2 = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("C: ");
+        int c2 = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine($"Uchburchaknign Yuzi : {a2 * b2 * c2}");
+    }
 }
-else if(belgi == "-"){
-    System.Console.WriteLine($"{a} - {b} = {a - b}");
+else if(menyu == "2"){
+    System.Console.WriteLine("Sizga nimaning chizmasi kerak?\n--------------------------------------\n");
+    System.Console.WriteLine("Turtburchak   |   Uchburchak   |   Kvadrat   |   ......");
+    string yunalish2 = Console.ReadLine();
+    if(yunalish2 == "1"){
+        System.Console.WriteLine("***********\n*         *\n*         *\n*         *\n*         *\n*         *\n***********\n");
+    }
+    else if(yunalish2 == "2"){
+        Console.WriteLine("       *\n     *  *\n    *    *\n   *      *\n  *        *\n *          *\n**************");
+    }
+    else if(yunalish2 == "3"){
+        Console.WriteLine("**********\n*        *\n*        *\n*        *\n**********\n");
+    }
 }
-else if(belgi == "*"){
-    System.Console.WriteLine($"{a} * {b} = {a * b}");
+else if(menyu == "3"){
+    ok:
+    System.Console.WriteLine("----------- Calculator -----------\n");
+    System.Console.Write("Sonni kiriting: ");
+    int a3 = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Amalni kiriting: ");
+    string amal = Console.ReadLine();
+    System.Console.Write("Sonni kiriting: ");
+    int b3 = Convert.ToInt32(Console.ReadLine());
+    if(amal == "+"){
+        System.Console.WriteLine("----------- Javob -----------");
+        System.Console.WriteLine($"{a3} + {b3} = {a3 + b3}");
+    }
+    else if(amal == "-"){
+        System.Console.WriteLine($"{a3} - {b3} = {a3 - b3}");
+    }
+    else if(amal == "*"){
+        System.Console.WriteLine($"{a3} * {b3} = {a3 * b3}");
+    }
+    else if(amal == "/"){
+        System.Console.WriteLine($"{a3} / {b3} = {a3 / b3}");
+    }
+
+    System.Console.WriteLine("\n---------------------------------\nCalculatordan yana foydalanasizmi?\n---------------------------------");
+    string s1 = Console.ReadLine();
+    if(s1 == "ha" || s1 == "yes"){
+        goto ok;
+    }
 }
-else if(belgi == "/"){
-    System.Console.WriteLine($"{a} / {b} = {a / b}");
+else if(menyu == "yordam" || menyu == "help"){
+    System.Console.WriteLine("Siz menyudagi suzlarni tanlash uchun sonlardan foydalanasiz.\nBu dastur sizga qayta qayta ishlab bera oladi.\nAgar dasturda belgilanmagan shart bersangiz siz dasturdan chiqib ketasiz.\n");
+    string d = Console.ReadLine();
+    goto salom;
 }
 else{
-    System.Console.WriteLine("Not found!");
-}   
+    
+}
 
-System.Console.Write("Shall we continue?: ");
+System.Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n---------------------------\nDasturni davom ettirasizmi?\n---------------------------\n");
 string s = Console.ReadLine();
-if(s == "yes"){
+
+if(s == "ha" || s == "yes" || s == "1"){
     goto hi;
 }
 else{
-    System.Console.WriteLine("Ok bro ;)");
+    System.Console.WriteLine(" ");
 }
-
-
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SimpleCalculator1
+# Bu dastur nimalar qila oldi?
+
++ Siz bunda xar xil amallarni bajara olasiz
+- Shakillarning chizmasini topa olasiz
++ Calculatordan foydalanishingiz mumkin
+
+- - -
+.  .  .  .  .  .
